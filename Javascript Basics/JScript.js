91ed504a9912cbcd38e17232bf83e525bb2ffe39
atharva <88647398+atharvahanjankar@users.noreply.github.com>
2022-01-08 12:07:22 +0530

    Add files via upload

--- Javascript Basics/JScript.js
+++ Javascript Basics/JScript.js
@@ -1,5 +1,38 @@
-var num1 = 10;
-var num2 = 20;
-var sum = num1 + num2;
-console.log(sum);
-document.getElementById("demo").innerHTML = sum;
+// var num1 = 10;
+// var num2 = 20;
+// var sum = num1 + num2 - (num1 * num2) / 2;
+// console.log("original num1: " + num1);
+// // var sum = ++num1; // num1++ = num1 + 1;
+// // sum = num1;
+// console.log("After decrement num1:" + num1);
+// console.log(sum);
+
+// document.getElementById("demo").innerHTML = sum;
+
+// var result = 5;
+// var num1 = 2;
+// result = result + num1;
+// result **= num1;
+
+// console.log(result);
+
+// binary 1 0
+// equal to-- > ==
+// assigned-- > =
+// equals --> ===
+
+var age = "undefined";
+var age1;
+if (age == age1) {
+  console.log("under 18");
+}
+// else if (age > 18 && age < 25) {
+//   console.log("abda");
+// }
+// else if () {
+// }
+// else {
+//   console.log("18+");
+// }
+
+// < | > | <= | >= | == | ===
