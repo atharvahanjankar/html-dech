50b4b72dcd856a8890a25434432243c7c056c30c
atharva <88647398+atharvahanjankar@users.noreply.github.com>
2022-01-09 12:15:11 +0530

    Add files via upload

---
+++ ST Batch 9th Jan/script.js
@@ -0,0 +1,35 @@
+// function Add(){
+//     var num1 = document.getElementById('Num1').valueAsNumber;
+//     if(Num1 == 1){
+
+//     }
+//     else if(Num1 == 2){
+
+//     }
+
+//     var num2 = document.getElementById('Num2').valueAsNumber;
+
+//     var result = num1 + num2;
+//     // console.log(result);
+//     document.getElementById('Sum').value = result;
+// }
+
+function checkIfVowels(){
+    var char1 = 'o';
+    switch(char1){
+        default: 
+            console.log("consonants");
+            break;
+        case (age < 18):
+            console.log('vowel 1'); 
+            break;
+        case 'e':
+            console.log('vowel 2');
+            break;
+        case 'i':
+            console.log('vowel 3');
+            break;
+        
+        
+    }
+}
