2bc55e31664dfbb4181be82ab05fc55b59f257eb
atharva <88647398+atharvahanjankar@users.noreply.github.com>
2022-01-15 22:49:18 -0800

    Add files via upload

---
+++ 15th Oct/script.js
@@ -0,0 +1,22 @@
+function Display(){
+    // const animals = ['cat', 'bat', 'rat', 'puppy']; 
+
+    // for(let x of animals){
+    //     console.log(x);
+    // }
+
+    // const Student = {
+    //     firstName:'Alex',
+    //     lastName: 'Carry',
+    //     age: 21
+    // };
+
+    // for(let x in Student){
+    //     console.log(Student[x]);
+    // }
+
+    // for(let i = 0; i<5;i++){
+    //     document.write(i + "<br>"); 
+    // }
+    document.write('<iframe src="D:\atharva\html\js\sumof10num\index.html" ></iframe>');
+} 
