5aacd7a654399693266b0e5f5cf04e8a1fa91fb1
atharva <88647398+atharvahanjankar@users.noreply.github.com>
2022-01-29 12:01:51 +0530

    Add files via upload

---
+++ constructor/index.js
@@ -0,0 +1,12 @@
+function Student() {
+  (FName = "John"), (LName = "Doe");
+  let result = Add();
+  console.log(FName + " " + LName);
+}
+
+const temp2 = new Student();
+// console.log(temp2);
+
+function Add() {
+  return 5 + 6;
+}
